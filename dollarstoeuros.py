0088f30ffeeeb1da0dc7c74597bee589e0aa76dc
Cadeschuettpelz <Cadeschuettpelz>
2023-11-16 11:45:43 -0600

    finish

---
+++ dollarstoeuros.py
@@ -0,0 +1,10 @@
+dollars = float(input("enter dollar amount to be converted: "))
+
+euro = dollars * .94540
+
+print("₤",euro)
+
+while True:
+    dollar = float(input("enter dollar amount to be converted:"))
+    euro = dollar * .94540
+    print("₤",str(euro))
